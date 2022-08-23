d5179adac86b9ca2baf5f092aa2acb85f3704a7c
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-08-23 15:05:39 +0200

    miglioramenti

--- js/script.js
+++ js/script.js
@@ -2,11 +2,17 @@ const question_1 = prompt("Quanti chilometri vuole percorrere?");
 const question_1AsNumber = parseInt(question_1);
 console.log("question_1AsNumber", question_1AsNumber);
 let chilometri = question_1AsNumber;
+let risposta_1 = `Chilometri: ${chilometri}km`;
+document.getElementById("chilometri").innerHTML = risposta_1;
+
 
 const question_2 = prompt("Quanti anni hai?");
 const question_2AsNumber = parseInt(question_2);
 console.log("question_2AsNumber", question_2AsNumber);
 let anni = question_2AsNumber;
+let risposta_2 = `Anni: ${anni}`;
+document.getElementById("anni").innerHTML = risposta_2;
+
 
 
 const prezzo_km = 0.21;
@@ -14,19 +20,19 @@ const sconto_minorenni = 0.2;
 const sconto_over65 = 0.4;
 
 if (anni >= 65) {
-    const prezzo_biglietto = chilometri * prezzo_km * (1 - sconto_over65);
-    const risposta = `Il tuo prezzo del biglietto è ${prezzo_biglietto}€`;
-    document.getElementById("mio_id").innerHTML = risposta;
+    let prezzo_biglietto = chilometri * prezzo_km * (1 - sconto_over65);
+    let risposta = `Il tuo prezzo del biglietto: ${prezzo_biglietto.toFixed(2)}€`;
+    document.getElementById("prezzo").innerHTML = risposta;
 }
 else if (anni < 18) {
     let prezzo_biglietto = chilometri * prezzo_km * (1 - sconto_minorenni);
-    let risposta = `Il tuo prezzo del biglietto è ${prezzo_biglietto}€`;
-    document.getElementById("mio_id").innerHTML = risposta;
+    let risposta = `Il tuo prezzo del biglietto: ${prezzo_biglietto.toFixed(2)}€`;
+    document.getElementById("prezzo").innerHTML = risposta;
 }
 else if ((anni < 65) && (anni >= 18)) {
     let prezzo_biglietto = chilometri * prezzo_km;
-    let risposta = `Il tuo prezzo del biglietto è ${prezzo_biglietto}€`;
-    document.getElementById("mio_id").innerHTML = risposta;
+    let risposta = `Il tuo prezzo del biglietto: ${prezzo_biglietto.toFixed(2)}€`;
+    document.getElementById("prezzo").innerHTML = risposta;
 }
 
 
